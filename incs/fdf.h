656ba5f3799da4e8e125c9604efcd0d7a9e7863f
Tetsuro Ando <teando@c2r7s9.42tokyo.jp>
2024-11-16 05:53:36 +0900

    add gnl module

--- incs/fdf.h
+++ incs/fdf.h
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 08:13:12 by teando            #+#    #+#             */
-/*   Updated: 2024/11/13 08:17:17 by teando           ###   ########.fr       */
+/*   Updated: 2024/11/16 05:45:15 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FDF_H
 
 # include "../libs/libft/libft.h"
+# include "../libs/get_next_line/get_next_line.h"
 # include <unistd.h>
 # include <math.h>
 # include <stddef.h>
